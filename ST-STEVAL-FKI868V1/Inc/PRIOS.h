26dba11f70b6a9e12c52adc992fba8d1a7f5c0a0
Erwan Martin <public@fzwte.net>
2020-04-21 12:12:24 +0200

    Move include files in the include directory.

--- ST-STEVAL-FKI868V1/Inc/PRIOS.h
+++ ST-STEVAL-FKI868V1/Inc/PRIOS.h
@@ -3,6 +3,7 @@
 
 #include <stdint.h>
 
+uint8_t decodePRIOSPayload(uint8_t *in, uint8_t payload_len, uint32_t key, uint8_t *out);
 uint8_t getMetricsFromPRIOSWMBusFrame(uint8_t *frame, uint32_t *total_consumption, uint32_t *last_month_total_consumption);
 
 #endif
